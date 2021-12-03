ef1f94b328ee92894cb87d77998f34e56f5fe706
Benny Özcetin <benny@five-star.dev>
2021-12-03 18:03:32 +0100

    add solution code

--- src/Day02.kt
+++ src/Day02.kt
@@ -37,12 +37,45 @@ fun main() {
                     depth += (aim * movement)
                 }
                 UP -> {
-//                    depth -= movement
                     aim -= movement
                 }
                 DOWN -> {
-//                    depth += movement
                     aim += movement
+                }
+            }
+        }
+        return horizontalPosition * depth
+    }
+
+    fun part1Solution(input: List<String>): Int {
+        var horizontalPosition = 0
+        var depth = 0
+        val operations = input.map { it.split(' ') }
+        for ((direction, amountString) in operations) {
+            val amount = amountString.toInt()
+            when(direction) {
+                "up" -> depth -= amount
+                "down" -> depth += amount
+                "forward" -> horizontalPosition += amount
+            }
+        }
+        return horizontalPosition * depth
+    }
+
+    data class Operation(val direction: String, val amount: Int)
+
+    fun part2Solution(input: List<String>): Int {
+        var horizontalPosition = 0
+        var depth = 0
+        var aim = 0
+        val operations = input.map { it.split(' ') }.map { Operation(it[0], it[1].toInt())  }
+        for ((direction, amount) in operations) {
+            when(direction) {
+                "up" -> aim -= amount
+                "down" -> aim += amount
+                "forward" -> {
+                    horizontalPosition += amount
+                    depth += (aim * amount)
                 }
             }
         }
@@ -53,12 +86,16 @@ fun main() {
     val testInput = readInput("Day02_test")
     println(part1(testInput))
     check(part1(testInput) == 150)
+    check(part1Solution(testInput) == 150)
 
     val testInput2 = readInput("Day02_test")
     println(part2(testInput2))
     check(part2(testInput2) == 900)
+    check(part2Solution(testInput2) == 900)
 
     val input = readInput("Day02")
     println(part1(input))
+    println(part1Solution(input))
     println(part2(input))
+    println(part2Solution(input))
 }
